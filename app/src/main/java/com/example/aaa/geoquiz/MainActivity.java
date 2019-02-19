ebcb35a116aadb25459831ba15976f66b9a44be6
AlekseichukA <alekseichuk4@gmail.com>
2019-02-19 15:04:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/aaa/geoquiz/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.aaa.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTrueButton = (Button) findViewById(R.id.button_true);
+        mFalseButton = (Button) findViewById(R.id.button_false);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
